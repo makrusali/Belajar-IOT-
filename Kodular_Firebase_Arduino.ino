e4bcc5e9ff23f6cfb83f07ea240fdd8df69b2021
unknown <makrus361@gmail.com>
2021-10-30 15:04:33 +0700

    upload file

---
+++ Kodular_Firebase_Arduino.ino
@@ -0,0 +1,64 @@
+/* masukkan library yang dibutuhkan */
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+/* buat object firebaseData dari class FirebaseData */
+FirebaseData firebaseData;
+
+void setup()
+{
+  /* atur pin D0 sebagai OUTPUT */
+  pinMode(D0, OUTPUT);
+  
+  /* mulai komunikasi serial dengan baudrate 115200 */
+  Serial.begin(115200);
+
+  /* mulai koneksi WIFI begin(SSID,PASSWORD) */
+  WiFi.begin("PAK JU", "gilangfajar");
+
+  /* tunggu hingga wifi terkoneksi */
+  Serial.print("Menghubungkan ke Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Terkoneksi dengan IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* mulai koneksi dengan firebase begin(database URL ,database Secret */
+  Firebase.begin("belajar-8c049-default-rtdb.firebaseio.com/", "8zCu1t12nylWBmtKmafHfRbOJDcCAvaYAncxd0m1");
+
+  /* jika WIFI terputus , hubungkan kembali */
+  Firebase.reconnectWiFi(true);
+ 
+}
+
+void loop()
+{
+  /* coba Ambil data dari FIREBASE di PATH "/belajar_kodular/kondisi_lampu" */
+  if(Firebase.getString(firebaseData, "/belajar_kodular/kondisi_lampu") == true)
+  {
+    /* jika berhasil tampung dan tampilkan data tersebut  */
+    String data_lampu = firebaseData.stringData();
+    
+    Serial.print("Ambil data string berhasil , = ");
+    Serial.println(data_lampu);
+
+    if(data_lampu == String("1"))
+    {
+      digitalWrite(D0, HIGH);
+    }
+    if(data_lampu == String("0"))
+    {
+      digitalWrite(D0, LOW);
+    }
+    delay(2000);
+  }else{
+    /* jika gagal tampilkan pesan error nya  */
+    Serial.print("Ambil data String eroor, ");
+    Serial.println(firebaseData.errorReason());
+  }
+}
